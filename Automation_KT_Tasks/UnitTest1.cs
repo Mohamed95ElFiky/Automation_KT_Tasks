703201f2d3a764582c7648c862846d7786b0abb9
Mohamed Elfiky <Mohamed.Elfiky@geidea.net>
2025-07-06 10:39:29 +0300

    inital commit

---
+++ Automation_KT_Tasks/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Automation_KT_Tasks
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
